04b6ce27f3cca3e2185839f6bd32064526b5e0fa
monkeyDugi <lbd4946@gmail.com>
2021-12-04 17:08:33 +0900

    chore : 사용하지 않는 import 제거

--- src/test/java/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculatorTest.java
@@ -1,8 +1,7 @@
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringAddCalculatorTest {
 
